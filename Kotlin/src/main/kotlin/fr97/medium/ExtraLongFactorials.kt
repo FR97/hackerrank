9394f16440545a7c423be7a454820b756b95f665
Filip <fristic97@gmail.com>
2020-06-19 14:14:41 +0200

    Extra Long Factorials solution.

---
+++ Kotlin/src/main/kotlin/fr97/medium/ExtraLongFactorials.kt
@@ -0,0 +1,21 @@
+package fr97.medium
+
+import java.math.*
+import java.util.*
+import kotlin.collections.*
+import kotlin.io.*
+import kotlin.text.*
+
+// Complete the extraLongFactorials function below.
+fun extraLongFactorials(n: Int): Unit {
+    val factorial = (1..n).fold(BigInteger.ONE) { a, b -> a.times(b.toBigInteger()) }
+    println(factorial)
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    extraLongFactorials(n)
+}
