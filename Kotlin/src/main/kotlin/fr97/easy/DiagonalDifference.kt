03885ad687dbc4067a1c81b88cccf7ce6ac8bb2c
Filip <fristic97@gmail.com>
2020-06-19 14:13:35 +0200

    Diagonal Difference solution.

---
+++ Kotlin/src/main/kotlin/fr97/easy/DiagonalDifference.kt
@@ -0,0 +1,33 @@
+package fr97.easy
+
+import kotlin.collections.*
+import kotlin.io.*
+import kotlin.math.abs
+import kotlin.ranges.*
+import kotlin.text.*
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+fun diagonalDifference(arr: Array<Array<Int>>): Int {
+    // Write your code here
+    return abs(arr.indices.sumBy { arr[it][it] - arr[it][arr.size - 1 - it] })
+}
+
+fun main(args: Array<String>) {
+    val n = readLine()!!.trim().toInt()
+
+    val arr = Array<Array<Int>>(n, { Array<Int>(n, { 0 }) })
+
+    for (i in 0 until n) {
+        arr[i] = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+    }
+
+    val result = diagonalDifference(arr)
+
+    println(result)
+}
