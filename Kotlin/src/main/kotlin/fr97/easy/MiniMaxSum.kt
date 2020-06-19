76636d570ad67a9baa2cfc7497f09b3eba18c87f
Filip <fristic97@gmail.com>
2020-06-19 14:30:55 +0200

    Mini Max Sum solution.

---
+++ Kotlin/src/main/kotlin/fr97/easy/MiniMaxSum.kt
@@ -0,0 +1,21 @@
+package fr97.easy
+
+import java.util.*
+import kotlin.collections.*
+import kotlin.io.*
+import kotlin.text.*
+
+fun miniMaxSum(arr: Array<Int>): Unit {
+    val arrLong = arr.map { it.toLong() }
+    val min = arrLong.sum() - arr.max()!!
+    val max = arrLong.sum() - arr.min()!!
+    println( "$min $max")
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val arr = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    miniMaxSum(arr)
+}
