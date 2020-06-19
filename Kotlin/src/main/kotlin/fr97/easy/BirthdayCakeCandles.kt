70147e361d1174b8126c7358ed3e07fa9f39e71b
Filip <fristic97@gmail.com>
2020-06-19 14:37:37 +0200

    Birth Day Cake Candles solution.

---
+++ Kotlin/src/main/kotlin/fr97/easy/BirthdayCakeCandles.kt
@@ -0,0 +1,24 @@
+package fr97.easy
+
+import java.util.*
+import kotlin.collections.*
+import kotlin.io.*
+import kotlin.text.*
+
+// Complete the birthdayCakeCandles function below.
+fun birthdayCakeCandles(ar: Array<Int>): Int {
+    val max = ar.max() ?: 0
+    return ar.filter { it == max }.size
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val arCount = scan.nextLine().trim().toInt()
+
+    val ar = scan.nextLine().split(" ").map{ it.trim().toInt() }.toTypedArray()
+
+    val result = birthdayCakeCandles(ar)
+
+    println(result)
+}
