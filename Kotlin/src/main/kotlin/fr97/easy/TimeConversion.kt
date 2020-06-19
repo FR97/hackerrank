419bba0fa0326142574eeef0e5a16b7b7d893acd
Filip <fristic97@gmail.com>
2020-06-19 15:06:38 +0200

    Time Conversion solution.

---
+++ Kotlin/src/main/kotlin/fr97/easy/TimeConversion.kt
@@ -0,0 +1,32 @@
+package fr97.easy
+
+import java.util.*
+
+fun timeConversion(s: String): String {
+    val amPm = s.substring(s.length - 2)
+    val time = s.substring(0, s.length - 2).split(":")
+    val hour = time[0].toInt()
+
+    return when (amPm) {
+        "AM" ->
+            if (hour == 12)
+                "00" + time.slice(1 until time.size).joinToString(":", prefix = ":")
+            else
+                time[0] + time.slice(1 until time.size).joinToString(":", prefix = ":")
+        else ->
+            if (hour == 12)
+                "12" + time.slice(1 until time.size).joinToString(":", prefix = ":")
+            else
+                (hour + 12).toString() + time.slice(1 until time.size).joinToString(":", prefix = ":")
+    }
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val s = scan.nextLine()
+
+    val result = timeConversion(s)
+
+    println(result)
+}
