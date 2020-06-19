54b5bf908d5752c975e2c9a869edfe607230977d
Filip <fristic97@gmail.com>
2020-06-19 14:11:01 +0200

    Compare The Triplets solution.

---
+++ Kotlin/src/main/kotlin/fr97/easy/CompareTheTriplets.kt
@@ -0,0 +1,27 @@
+package fr97.easy
+
+import kotlin.collections.*
+import kotlin.io.*
+
+import kotlin.text.*
+
+// Complete the compareTriplets function below.
+fun compareTriplets(a: Array<Int>, b: Array<Int>): Array<Int> {
+    return a.mapIndexed { i, score -> score.compareTo(b[i]) }
+        .filter { it != 0 }
+        .fold(arrayOf(0,0)) { arr, sc ->
+            if(sc > 0) arr[0] = arr[0]+1
+            else arr[1] = arr[1]+1
+            arr
+        }
+}
+
+fun main(args: Array<String>) {
+    val a = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val b = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = compareTriplets(a, b)
+
+    println(result.joinToString(" "))
+}
