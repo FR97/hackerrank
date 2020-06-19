0deddc4377f4ae3aaca40110ac165de3c530149f
Filip <fristic97@gmail.com>
2020-06-19 14:10:41 +0200

    Simple Array Sum solution.

---
+++ Kotlin/src/main/kotlin/fr97/easy/SimpleArraySum.kt
@@ -0,0 +1,26 @@
+package fr97.easy
+
+
+import java.util.*
+
+/*
+ * Complete the simpleArraySum function below.
+ */
+fun simpleArraySum(ar: Array<Int>): Int {
+    /*
+     * Write your code here.
+     */
+    return ar.sum()
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val arCount = scan.nextLine().trim().toInt()
+
+    val ar = scan.nextLine().split(" ").map { it.trim().toInt() }.toTypedArray()
+
+    val result = simpleArraySum(ar)
+
+    println(result)
+}
