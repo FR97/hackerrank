b9ba16229fc6fb3437153a2c5920e712d3af608b
Filip <fristic97@gmail.com>
2020-06-23 23:53:02 +0200

    Minimum Loss Solution.

---
+++ Kotlin/src/main/kotlin/fr97/medium/MinimumLoss.kt
@@ -0,0 +1,37 @@
+package fr97.medium
+
+
+import java.util.*
+import kotlin.collections.*
+import kotlin.io.*
+import kotlin.text.*
+
+// Complete the minimumLoss function below.
+fun minimumLoss(price: Array<Long>): Int {
+
+    val mappedByPrice = price.mapIndexed { i, p -> p to i.toLong() }.toMap()
+
+    println(mappedByPrice)
+
+    return price
+        .sortedArray()
+        .foldIndexed(Long.MAX_VALUE) { i, min, p ->
+            val prevPrice = if (i == 0) 0 else price[i-1]
+            if (p - prevPrice in 1 until min)
+                p - prevPrice
+            else
+                min
+        }.toInt()
+}
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val n = scan.nextLine().trim().toInt()
+
+    val price = scan.nextLine().split(" ").map { it.trim().toLong() }.toTypedArray()
+
+    val result = minimumLoss(price)
+
+    println(result)
+}
