95f528428905153cfc4e452578708986d6722be8
Filip <fristic97@gmail.com>
2020-06-24 17:02:16 +0200

    Pairs imperative solution.

--- Kotlin/src/main/kotlin/fr97/medium/Pairs.kt
+++ Kotlin/src/main/kotlin/fr97/medium/Pairs.kt
@@ -7,8 +7,29 @@ import kotlin.math.abs
 import kotlin.text.*
 
 
-
 fun pairs(k: Int, arr: Array<Int>): Int {
+    arr.sort()
+
+    var i = 1
+    var j = 0
+    var totalPairs = 0
+
+    while (i < arr.size) {
+        when {
+            arr[i] - arr[j] == k -> {
+                totalPairs++; j++
+            }
+            arr[i] - arr[j] < k -> i++
+            else -> j++
+        }
+    }
+
+    return totalPairs
+}
+
+// Functional solution, problem is it is too slow to pass hackerrank test
+// TODO: try to improve performance to avoid timeout
+fun pairsFunctional(k: Int, arr: Array<Int>): Int {
     val allPairs = arr
         .mapIndexed { i1, el1 -> arr.filterIndexed { i2, el2 -> i1 != i2 && abs(el1 - el2) == k }.size }
         .sum()
